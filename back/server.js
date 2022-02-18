90e9497bbb6695903a24d270e1c5ceb0abb7fc2e
Justinas <Justinas@Justinass-MacBook-Pro.local>
2022-02-18 20:01:06 +0100

    first update

--- back/server.js
+++ back/server.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const cors = require('cors')
-
+const mongoose = require('mongoose')
 const app = express();
+
 app.use(cors())
+app.use(express.json())
 app.listen(4000)
+
+mongoose.connect("mongodb+srv://adminas1:ponasadminas@cluster0.qqies.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    .then(res => {
+        console.log('connection good')
+    }).catch(e => {
+    console.log(e)
+})
+
+const router = require("./routes/mainRoutes")
+app.use("/", router)
+
